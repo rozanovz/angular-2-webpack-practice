e0d3d22576f97d49c713f3c9a99f40609e3bfa28
rozanovz <rozanovz@gmail.com>
2016-06-14 19:17:16 +0300

    initial

---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/chartjs/chart.d.ts" />
+/// <reference path="globals/core-js/index.d.ts" />
+/// <reference path="globals/jasmine/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
